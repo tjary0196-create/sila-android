98dcfa7db5751e9b0f1859903469fd84c3f548eb
tjary0196-create <tjary0196@gmail.com>
2026-08-02 08:20:01 +0300

    Refactor build.gradle.kts for cleaner syntax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,17 +28,13 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "0.1.0"
-
         buildConfigField("String", "WEB_CLIENT_ID", "\"$webClientId\"")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
@@ -46,21 +42,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
     buildFeatures {
         compose = true
         buildConfig = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.4" }
+    packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
 
 dependencies {
@@ -70,13 +58,12 @@ dependencies {
 
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material3:material3:$material3Version")
+    implementation("androidx.compose.material:material-icons-core:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-
     implementation("androidx.navigation:navigation-compose:2.7.5")
-
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation(platform("com.google.firebase:firebase-bom:$firebaseBom"))
@@ -84,6 +71,5 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx")
 
     implementation("com.google.android.gms:play-services-auth:21.4.0")
-
     implementation("com.google.code.gson:gson:2.10.1")
 }
