7ef9dbb509d14f604f95e968dcc3adb3494145e9
tjary0196-create <tjary0196@gmail.com>
2026-08-05 10:58:07 +0300

    Delete app/src/main/java/com/sila/messaging/model/Models.kt

--- app/src/main/java/com/sila/messaging/model/Models.kt
+++
@@ -1,8 +0,0 @@
-package com.sila.messaging.model
-
-data class SimpleUser(
-    val uid: String = "",
-    val username: String = "",
-    val displayName: String? = null,
-    val photoUrl: String? = null
-)
